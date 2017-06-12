eb01076a30107040c8a1547c2c25a395cc48518e
xolani <xolani@ben-laptop>
2017-06-12 22:53:12 +0100

    Initial project setup.

---
+++ src/GSM/GSMSession.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GSM;
+
+/**
+ *
+ * @author xolani
+ */
+public class GSMSession {
+    
+    public GSMSession()
+    {
+        
+    }
+    
+    public String[] getSMS(int index)
+    {
+        String[] SMS = new String[2];
+        return SMS;
+    }
+    
+    public void sendSMS(String telNumber, String SMS)
+    {
+        if (SMS.length() <= 160)
+        {
+            
+        }
+        else if (SMS.length() <= 39015)
+        {
+            
+        }
+        else
+        {
+            
+        }
+    }
+    
+    public String getClock()
+    {
+        String time = "";
+        
+        return time;
+    }
+}
